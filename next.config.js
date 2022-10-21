a179d965c0fb8bb00f1dc4efd369cfa200046b6e
Min Thet Naing <bobo.b90@gmail.com>
2022-10-21 16:37:50 +0630

    Create initial pages for admin

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    domains: ["source.unsplash.com"]
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import AdminLayout from '../components/admin_layout'
 
-function MyApp({ Component, pageProps }) {
+function Liveheats({ Component, pageProps, router }) {
+  // if (router.pathname.startsWith('/admin')) {
+  //   return (
+  //     <AdminLayout>
+  //       <Component {...pageProps} />
+  //     </AdminLayout>
+  //   )
+  // }
+
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default Liveheats
